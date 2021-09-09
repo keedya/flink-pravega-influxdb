a1eed341f3b892424e39d265848edf4d3ee477ed
keedya <andre.keedy@dell.com>
2021-09-09 19:02:42 -0400

    Use jdbc sink for timescaleDB

--- flinkprocessor/src/main/java/io/pravega/flinkprocessor/PravegaToTimeScaleDB.java
+++ flinkprocessor/src/main/java/io/pravega/flinkprocessor/PravegaToTimeScaleDB.java
@@ -4,18 +4,17 @@ import io.pravega.client.stream.StreamCut;
 import io.pravega.connectors.flink.FlinkPravegaReader;
 import io.pravega.flinkprocessor.datatypes.tag;
 import io.pravega.flinkprocessor.util.JsonDeserializationSchema;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.io.jdbc.JDBCOutputFormat;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.types.Row;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Types;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-
 
 public class PravegaToTimeScaleDB extends AbstractJob {
 
@@ -37,21 +36,6 @@ public class PravegaToTimeScaleDB extends AbstractJob {
 
     public void run() {
         try {
-            String query = String.format("INSERT INTO %s(time, tagname, devicename, deviceid, value, success) VALUES (?, ?, ?, ?, ?, ?)",
-                    getConfig().getTimescaledbTable());
-            JDBCOutputFormat jdbcOutput = JDBCOutputFormat.buildJDBCOutputFormat()
-                    .setDrivername("org.postgresql.Driver")
-                    .setDBUrl(String.format("jdbc:%s/%s?user=%s&password=%s",
-                            getConfig().getTimescaledbUrl(),
-                            getConfig().getTimescaledbDatabase(),
-                            getConfig().getTimescaledbUsername(),
-                            getConfig().getTimescaledbPassword()
-                             )
-                    )
-                    .setQuery(query)
-                    .setSqlTypes(new int[] { Types.TIMESTAMP_WITH_TIMEZONE, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.BIGINT, Types.BOOLEAN })
-                    .setBatchInterval(getConfig().getTimescaledbBatchSize())
-                    .finish();
             final AppConfiguration.StreamConfig inputStreamConfig = getConfig().getStreamConfig("input");
             log.info("input stream: {}", inputStreamConfig);
             createStream(inputStreamConfig);
@@ -70,18 +54,32 @@ public class PravegaToTimeScaleDB extends AbstractJob {
             DataStream<tag> events = env
                     .addSource(flinkPravegaReader)
                     .name("read-flatten-events");
-            //events.printToErr();
-            DataStream<Row> rows = events.map((MapFunction<tag, Row>) metricValue -> {
-                Row row = new Row(6);
-                row.setField(0, LocalDateTime.ofEpochSecond(metricValue.timestamp/1000, 0 , ZoneOffset.UTC));
-                row.setField(1, metricValue.tagName);
-                row.setField(2, metricValue.deviceName);
-                row.setField(3, metricValue.deviceID);
-                row.setField(4, metricValue.value);
-                row.setField(5, metricValue.success);
-                return row;
-            });
-            rows.writeUsingOutputFormat(jdbcOutput);
+            events.addSink(
+                    JdbcSink.sink(
+                            "INSERT INTO litmus(time, tagname, devicename, deviceid, value, success) VALUES (?, ?, ?, ?, ?, ?)",
+                            (statement, metricValue) -> {
+                                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.ofEpochSecond(metricValue.timestamp/1000, 0 , ZoneOffset.UTC)));
+                                statement.setString(2, metricValue.tagName);
+                                statement.setString(3, metricValue.deviceName);
+                                statement.setString(4, metricValue.deviceID);
+                                statement.setDouble(5, metricValue.value);
+                                statement.setBoolean(6, metricValue.success);
+                                },
+                            JdbcExecutionOptions.builder()
+                                    .withBatchSize(getConfig().getInfluxdbBatchSize())
+                                    .withBatchIntervalMs(1000)
+                                    .withMaxRetries(5)
+                                    .build(),
+                            new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                                    .withUrl(String.format("jdbc:%s/%s",
+                                            getConfig().getTimescaledbUrl(),
+                                            getConfig().getTimescaledbDatabase()
+                                    ))
+                                    .withDriverName("org.postgresql.Driver")
+                                    .withUsername(getConfig().getTimescaledbUsername())
+                                    .withPassword(getConfig().getTimescaledbPassword())
+                                    .build()
+                            ));
 
             env.execute(PravegaToTimeScaleDB.class.getSimpleName());
         } catch (Exception e) {
